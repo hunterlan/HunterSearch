3710684c6d09d129e2d7c42787a4a3100873e132
hunterlan <konstantin.sharykin@outlook.com>
2024-01-03 17:38:45 +0100

    Init commit

---
+++ HunterSearch.Console/Program.cs
@@ -0,0 +1,166 @@
+﻿using Elastic.Clients.Elasticsearch;
+using System.Runtime.InteropServices;
+
+[DllImport("kernel32.dll")]
+static extern bool GetSystemTimes(
+    out FILETIME lpIdleTime,
+    out FILETIME lpKernelTime,
+    out FILETIME lpUserTime
+);
+
+[DllImport("kernel32.dll")]
+[return: MarshalAs(UnmanagedType.Bool)]
+static extern bool FileTimeToSystemTime(
+    ref FILETIME lpFileTime,
+    out SYSTEMTIME lpSystemTime
+);
+
+[DllImport("kernel32.dll", SetLastError = true)]
+[return: MarshalAs(UnmanagedType.Bool)]
+static extern bool GlobalMemoryStatusEx(ref MEMORYSTATUSEX lpBuffer);
+
+const string hostUrl = "http://localhost:9200";
+const string name = "s28619_performance_data";
+
+var settings = new ElasticsearchClientSettings(new Uri(hostUrl));
+var client = new ElasticsearchClient(settings);
+
+var pingOperation = client.PingAsync();
+var dotCounter = 0;
+
+while (!pingOperation.IsCompleted)
+{
+    switch (dotCounter)
+    {
+        case 0:
+            Console.Write("Trying to connect to " + hostUrl);
+            break;
+        case 4:
+            Console.Clear();
+            dotCounter = -1;
+            break;
+        default:
+            Console.Write(".");
+            Thread.Sleep(500);
+            break;
+    }
+
+    dotCounter++;
+}
+Console.Write("\n");
+
+var pingResponse = pingOperation.Result;
+
+if (pingResponse.IsSuccess())
+{
+    Console.WriteLine("Connection is established.");
+}
+else
+{
+    Console.WriteLine("Failed to connect. Continuous execution isn't possible.");
+    Environment.Exit(0);
+}
+
+Console.WriteLine();
+var creationOperation = await client.Indices.CreateAsync(name);
+
+if (creationOperation.IsSuccess())
+{
+    var countMinutes = 0;
+    while (countMinutes != 15)
+    {
+        var dateTime = DateTime.Now;
+        var timestamp = new DateTimeOffset(dateTime).ToUnixTimeSeconds();
+        var ip = "0.0.0.0";
+        var cpuUsage = GetCpuUsage();
+        var ramUsage = GetRamUsage();
+        Console.WriteLine($"CPU usage on {countMinutes + 1} minute is {cpuUsage}%");
+        Console.WriteLine($"RAM usage on {countMinutes + 1} minute is {ramUsage} GB");
+        Console.WriteLine("");
+        var currentDeviceInfo = new DeviceInfo(timestamp, ip, cpuUsage, ramUsage);
+        var sendResponse = await client.IndexAsync(currentDeviceInfo, "");
+        Thread.Sleep(60000);
+        countMinutes++;
+    }
+}
+else
+{
+    Console.WriteLine("Failed to create new index. Continuous execution isn't possible.");
+    Environment.Exit(0);
+}
+
+return;
+/*
+ * Only God and MSDN knows how it works, WinAPI is cursed thing 💀
+ */
+ulong GetRamUsage()
+{
+    var memoryStatus = new MEMORYSTATUSEX();
+    memoryStatus.dwLength = (uint)Marshal.SizeOf(typeof(MEMORYSTATUSEX));
+
+    var totalMemoryGb = memoryStatus.ullTotalPhys / (1024 * 1024 * 1024);
+    var availableMemoryGb = memoryStatus.ullAvailPhys / (1024 * 1024 * 1024);
+
+    return totalMemoryGb - availableMemoryGb;
+}
+
+double GetCpuUsage()
+{
+    FILETIME idleTime, kernelTime, userTime;
+    GetSystemTimes(out idleTime, out kernelTime, out userTime);
+
+    SYSTEMTIME idleSystemTime, kernelSystemTime, userSystemTime;
+    FileTimeToSystemTime(ref idleTime, out idleSystemTime);
+    FileTimeToSystemTime(ref kernelTime, out kernelSystemTime);
+    FileTimeToSystemTime(ref userTime, out userSystemTime);
+
+    long idleTicks = (((long)idleTime.dwHighDateTime) << 32) | idleTime.dwLowDateTime;
+    long kernelTicks = (((long)kernelTime.dwHighDateTime) << 32) | kernelTime.dwLowDateTime;
+    long userTicks = (((long)userTime.dwHighDateTime) << 32) | userTime.dwLowDateTime;
+
+    double idlePercent = (double)idleTicks / (kernelTicks + userTicks) * 100;
+
+    return 100 - idlePercent;
+}
+
+internal class DeviceInfo(long currentTimestamp, string ipAddress, double cpuUsage, double ramUsage)
+{
+    public long CurrentTimestamp { get; set; } = currentTimestamp;
+    public string IpAddress { get; set; } = ipAddress;
+    public double CpuUsage { get; set; } = cpuUsage;
+    public double RamUsage { get; set; } = ramUsage;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+struct SYSTEMTIME
+{
+    public ushort wYear;
+    public ushort wMonth;
+    public ushort wDayOfWeek;
+    public ushort wDay;
+    public ushort wHour;
+    public ushort wMinute;
+    public ushort wSecond;
+    public ushort wMilliseconds;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+struct FILETIME
+{
+    public uint dwLowDateTime;
+    public uint dwHighDateTime;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+struct MEMORYSTATUSEX
+{
+    public uint dwLength;
+    public uint dwMemoryLoad;
+    public ulong ullTotalPhys;
+    public ulong ullAvailPhys;
+    public ulong ullTotalPageFile;
+    public ulong ullAvailPageFile;
+    public ulong ullTotalVirtual;
+    public ulong ullAvailVirtual;
+    public ulong ullAvailExtendedVirtual;
+}
